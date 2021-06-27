37b79d71f9d585d3bdcb463130ca1df1b1dedaba
kimjongkeun <jongkeun@godo.co.kr>
2021-06-27 18:36:19 +0900

    Add fromEvent bind data

--- src/app.js
+++ src/app.js
@@ -3,19 +3,29 @@ import { map, filter, switchMap } from 'https://dev.jspm.io/rxjs@6/_esm2015/oper
 
 (() => {
   const immigration = {
-    dom: {
-      form: document.getElementById('form'),
+    data: {
+      galaxy: null
     },
     observer: {
-      form: fromEvent(document, 'click')
+      form: fromEvent(document.getElementById('form'), 'submit'),
+      select: {
+        galaxy: fromEvent(document.getElementById('galaxy'), 'change')
+      }
     },
     initiate() {
       this.bindEvents();
     },
     bindEvents() {
       this.observer.form.subscribe(this.event.submit)
+      this.observer.select.galaxy.subscribe(this.event.pick.galaxy)
     },
     event: {
+      pick: {
+        galaxy(event) {
+          immigration.data.galaxy = event.target.value
+          console.log(immigration.data)
+        },
+      },
       submit(event) {
         event.preventDefault()
 
